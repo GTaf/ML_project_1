4fd3e551facd6b1aa250aa562d21342be400f43e
GTaf <auredu94@gmail.com>
2018-10-11 12:30:19 +0200

    Score over 0.8

--- implementations.py
+++ implementations.py
@@ -55,3 +55,20 @@ def ridge_regression(y, tx, lamb):
     a = tx.T.dot(tx) + aI
     b = tx.T.dot(y)
     return np.linalg.solve(a, b)
+
+def split_data(x, y, ratio, myseed=1):
+    """split the dataset based on the split ratio."""
+    # set seed
+    np.random.seed(myseed)
+    # generate random indices
+    num_row = len(y)
+    indices = np.random.permutation(num_row)
+    index_split = int(np.floor(ratio * num_row))
+    index_tr = indices[: index_split]
+    index_te = indices[index_split:]
+    # create split
+    x_tr = x[index_tr]
+    x_te = x[index_te]
+    y_tr = y[index_tr]
+    y_te = y[index_te]
+    return x_tr, x_te, y_tr, y_te
