7b6d200f72463e475ba9cc9ff74af3e3cb68e6df
Thomas PETITJEAN <thomas.petit-jean@epfl.ch>
2018-10-19 16:54:28 +0200

    CC

--- implementations.py
+++ implementations.py
@@ -1,34 +1,42 @@
 import numpy as np
 
+
 def calculate_mse(e):
     """Calculate the mse for vector e"""
     return (1./2)*np.mean(e**2)
 
 def compute_loss(y, tx, w):
     """Calculate the loss using mse"""
-    e = y - tx.dot(w)
+    e = (y - tx.dot(w))
     return calculate_mse(e)
 
-def least_squares(y, tx): 
+def least_squares(y, tx,computeLoss = True): 
     """calculate the least squares solution using normal equations"""  
     a = tx.T.dot(tx)
     b = tx.T.dot(y)
     w = np.linalg.solve(a, b)
-    loss = compute_loss(y, tx, w)
-    return w, loss
+    if computeLoss:
+        loss = compute_loss(y, tx, w)
+        return w, loss
+    else:
+        return w
 
 def compute_gradient(y, tx, w):
     """Compute the gradient"""
     N = int(y.shape[0])
     e = y - tx.dot(w)
-    return np.transpose(tx).dot(e)/(-N)
+    print N
+    return -(np.transpose(tx).dot(e))/(N)
 
 def least_squares_GD(y, tx, initial_w, max_iters, gamma,computeLoss = True):
     """Gradient descent algorithm."""
     w = initial_w
     for n_iter in range(max_iters):
+        
         grad = compute_gradient(y, tx, w)
+        print grad
         w = w - gamma*grad
+        print w
     if computeLoss:
         loss = compute_loss(y, tx, w)
         return w,loss
@@ -47,11 +55,11 @@ def stochastic_gradient_descent1(y, tx, initial_w, max_iters, gamma):
        
     return w,loss
 
-
-
-def ridge_regression(y, tx, lamb):
-    """implement ridge regression."""
-    aI = lamb * np.identity(tx.shape[1])
-    a = tx.T.dot(tx) + aI
+def ridge_regression(y, tx, lambda_): 
+    """calculate the ridge regression solution using normal equations"""  
+    N = int(tx.shape[0])
+    a = tx.T.dot(tx) + 2*N*lambda_*np.identity(tx.shape[1])
     b = tx.T.dot(y)
-    return np.linalg.solve(a, b)
+    w = np.linalg.solve(a, b)
+    loss = compute_loss(y, tx, w)
+    return w,loss
